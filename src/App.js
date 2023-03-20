f3e04e5ead6951720252eb63c76b5a2f29667a76
Kurmanbek0_0 <mr.tufio@gmail.com>
2023-03-20 17:48:39 +0600

    dfcv

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import './App.css';
-<<<<<<< HEAD
-import Modern from './components/modern/Modern';
+import React from "react";
+import Modern from "./components/modern/Modern";
 
-function App() {
-  return (
-   <div>
-      <Modern/>
-   </div>
-=======
-import About from "./components/about";
-import Menu from "./components/Menu";
-
-function App() {
-  return (
-    <div className="App">
-        <About/>
-        <Menu/>
-    </div>
->>>>>>> bbcee19271486bd2ecf933dcd7392f254a0763d4
-  );
+function App(){
+  return(
+    <Modern/>
+  )
 }
-
-export default App;
